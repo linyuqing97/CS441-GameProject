497ba2ddc496cba9284f0f480010bc1415094928
j <j@DESKTOP-M0RSPA0>
2019-07-21 00:48:27 -0400

    add* scrollview,html,button to the game

--- app/src/main/java/com/example/myfungame/MainActivity.java
+++ app/src/main/java/com/example/myfungame/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.myfungame;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.sufficientlysecure.htmltextview.HtmlAssetsImageGetter;
+import org.sufficientlysecure.htmltextview.HtmlTextView;
+
 public class MainActivity extends AppCompatActivity {
 
-    String htmlCode = "<h2>Super Hero </h2>"
-                        +
+    String htmlCode = "<h2>Super Hero </h2>"+
+            "<h2>Super Hero </h2>"+"<h2>Super Hero </h2>"+                       "k asdklf jas" +
+            "<h2>Super Hero </h2>"+            "df jakl;'sdf ka'sdk;f as" +
+            "<h2>Super Hero </h2>"+
+            "<h2>Super Hero </h2>"+            "lkdf al;sdkf as;ldkf a;slkdf ;asldkf as" +
+            "<h2>Super Hero </h2>"+            "df kl;a's; dl;askf a's;ldk " +
+            "<h2>Super Hero </h2>"+            "akdfl; asdl;kf a" +
+            "<h2>Super Hero </h2>"+            "df kasld;fk a;sdlkf as" +
+            "<h2>Super Hero </h2>"+            "dfk asl;dk fas" +
+            "<h2>Super Hero </h2>"+            "'dpfk al;skdf a" +
+            "sdfk al;sdk f" +
+            "asdfkl asl</p>";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HtmlTextView htmlTextView = findViewById(R.id.html_text);
+        htmlTextView.setHtml(htmlCode,new HtmlAssetsImageGetter(htmlTextView));
     }
 }
