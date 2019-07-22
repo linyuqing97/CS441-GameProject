61d175dc94787abeb406fafd8021cc04615d86d9
j <j@DESKTOP-M0RSPA0>
2019-07-22 00:32:16 -0400

    add save and load data

--- app/src/main/java/com/example/myfungame/LeaderBoard.java
+++ app/src/main/java/com/example/myfungame/LeaderBoard.java
@@ -4,36 +4,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class LeaderBoard extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter myAdapter;
     RecyclerView.LayoutManager layoutManager;
-    ArrayList userRank;
+    ArrayList userRank = new ArrayList<userInfo>();;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
 
-        userRank = new ArrayList<userInfo>();
+
+        loadData();
 
         recyclerView = findViewById(R.id.recycleView);
-        layoutManager= new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         myAdapter = new RecycleAdapter(userRank);
         recyclerView.setAdapter(myAdapter);
 
-        if(getIntent().hasExtra("userName")) {
+        if (getIntent().hasExtra("userName")) {
+            System.out.println("new user"+ userRank.size());
             String userName = getIntent().getExtras().getString("userName");
             userInfo info = new userInfo(userName);
+
             userRank.add(info);
             myAdapter.notifyDataSetChanged();
 
+        }
 
+    }
+
+    public void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userRank);
+        editor.putString("Input list", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String  json = sharedPreferences.getString("Input list", null);
+        Type type = new TypeToken<ArrayList<userInfo>>() {
+        }.getType();
+        userRank = gson.fromJson(json, type);
+
+        if (userRank == null) {
+            userRank = new ArrayList<>();
         }
     }
 }
