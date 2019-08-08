ccc04e2bd38e07a2ebec19e7091e73fe42a9e5f8
Yuqing Lin <linyuqing97@gmail.com>
2019-08-08 00:06:22 -0400

    now can get scores from web

--- app/src/main/java/com/example/myfungame/LeaderBoard.java
+++ app/src/main/java/com/example/myfungame/LeaderBoard.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -39,54 +40,54 @@ interface NetResponse{
     void netResult(Integer code, JSONArray json);
 }
 
-public class LeaderBoard extends AppCompatActivity implements NetResponse  {
+public class LeaderBoard extends AppCompatActivity implements NetResponse {
     RecyclerView recyclerView;
     RecyclerView.Adapter myAdapter;
     RecyclerView.LayoutManager layoutManager;
-    public ArrayList<UserInfo>userRank;
+    public ArrayList<UserInfo> userRank;
     String updateString;
     Button webButton;
+    Button getWebButton;
     NetTask netTask;
     LeaderBoard handle;
     URL url;
-
+    String gameNameFromWeb,playerNameFromWeb;
+    int scoreFromWeb;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
         Button clearButton = findViewById(R.id.clearButton);
         webButton = findViewById(R.id.webButton);
+        getWebButton = findViewById(R.id.getWebResult);
         //userRank = new ArrayList<>();
 
         loadData();
 
         recyclerView = findViewById(R.id.recycleView);
-        recyclerView.scrollToPosition(userRank.size()-1);
+        recyclerView.scrollToPosition(userRank.size() - 1);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         myAdapter = new RecycleAdapter(userRank);
         recyclerView.setAdapter(myAdapter);
 
-         handle = this;
+        handle = this;
         webButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 System.out.println("Clicked the web button");
+                String userName = userRank.get(userRank.size() - 1).getName();
 
-                String tempString = "http://cs.binghamton.edu/~pmadden/courses/441score/postscore.php?player="+userRank.get(userRank.size()-1).getName()+"&game=AppleGo&score=";
-                int temp = userRank.get(userRank.size()-1).getPoint();
+                String tempString = "http://cs.binghamton.edu/~pmadden/courses/441score/postscore.php?player=".concat(userName).concat("&game=AppleGo&score=");
+                int temp = userRank.get(userRank.size() - 1).getPoint();
                 String request = tempString.concat(Integer.toString(temp));
-                try {
-                    url = new URL(request);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
+                System.out.println(request);
 
 
-                netTask = new NetTask("http://cs.binghamton.edu/~pmadden/courses/441score/getscores.php"
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(request));
+                startActivity(browserIntent);
 
-, request, handle);
 
-                netTask.execute((Void) null);
             }
         });
 
@@ -100,24 +101,27 @@ public class LeaderBoard extends AppCompatActivity implements NetResponse  {
 
 
         if (getIntent().hasExtra("userName") && getIntent().hasExtra("score")) {
-            System.out.println("new user"+ userRank.size());
+            System.out.println("new user" + userRank.size());
             String userName = getIntent().getExtras().getString("userName");
             int score = Integer.parseInt(getIntent().getExtras().getString("score"));
-            System.out.println("Score: "+score);
-            UserInfo info = new UserInfo(userName,"Apple go",score);
+            System.out.println("Score: " + score);
+            UserInfo info = new UserInfo(userName, "Apple go", score);
             userRank = showTopScore(info);
             myAdapter.notifyDataSetChanged();
 
-            }
-
-
-
-
-
-
         }
 
+        getWebButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                netTask = new NetTask("http://cs.binghamton.edu/~pmadden/courses/441score/getscores.php", null, handle);
 
+                netTask.execute((Void) null);
+            }
+        });
+
+
+    }
 
 
     public void onPause() {
@@ -134,28 +138,25 @@ public class LeaderBoard extends AppCompatActivity implements NetResponse  {
         editor.apply();
     }
 
-    public ArrayList<UserInfo> showTopScore (UserInfo temp){
+    public ArrayList<UserInfo> showTopScore(UserInfo temp) {
 
-        if (userRank.size()>20){
+        if (userRank.size() > 20) {
             userRank.remove(20);
             userRank.add(temp);
-        }
-        else{
+        } else {
             userRank.add(temp);
         }
         LeaderBoardShorter leaderBoardShorter = new LeaderBoardShorter(userRank);
         return leaderBoardShorter.getSortedLeaderBoard();
 
 
-
     }
-
 
 
     private void loadData() {
         SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
         Gson gson = new Gson();
-        String  json = sharedPreferences.getString("Input list", null);
+        String json = sharedPreferences.getString("Input list", null);
         Type type = new TypeToken<ArrayList<UserInfo>>() {
         }.getType();
         userRank = gson.fromJson(json, type);
@@ -164,27 +165,32 @@ public class LeaderBoard extends AppCompatActivity implements NetResponse  {
             userRank = new ArrayList<>();
         }
     }
-    public void netResult(Integer code, JSONArray json)
-    {
+
+    //
+    public void netResult(Integer code, JSONArray json) {
         System.out.println("Got a result from the web");
+        System.out.println(json.length());
         updateString = "";
 
-        for (int i = 0; i < json.length(); ++i)
-        {
+
+        for (int i = 0; i < json.length(); ++i) {
             System.out.println("Looping");
             try {
                 JSONObject item = json.getJSONObject(i);
 
-                if (item.getString("result") != null) {
+                if (item.getString("player") != null) {
                     System.out.println("Found a match");
-                    System.out.println(item.getString("result"));
-                    updateString = item.getString("result");
+
+//                    System.out.println(item.getString("player"));
+//                    updateString = item.getString("player");
+                    playerNameFromWeb = item.getString("player");
+                    gameNameFromWeb = item.getString("game");
+                    scoreFromWeb = Integer.parseInt(item.getString("score"));
 
 
                 }
-            }
-            catch (JSONException e)
-            {
+            } catch (JSONException e) {
+                System.out.println(e);
                 updateString = "JSON Error!";
             }
 
@@ -192,103 +198,104 @@ public class LeaderBoard extends AppCompatActivity implements NetResponse  {
                 @Override
                 public void run() {
 
-                    System.out.println("String connet is now : "+updateString);
+                    UserInfo info = new UserInfo(playerNameFromWeb, gameNameFromWeb,scoreFromWeb);
+                    userRank = showTopScore(info);
+                    myAdapter.notifyDataSetChanged();
 
                 }
             });
         }
     }
-}
+    class NetTask extends AsyncTask<Void, Void, Boolean> {
+            private final String urlString;
+            private final String reqString;
+            private NetResponse changeListener;
 
-
-
-
-class NetTask extends AsyncTask<Void, Void, Boolean> {
-        private final String urlString;
-        private final String reqString;
-        private NetResponse changeListener;
-
-        NetTask(String url, String request, NetResponse responseListener) {
-            urlString = url; reqString = request; changeListener = responseListener;
-        }
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            try {
-                System.out.println("JSON Query: " + reqString);
-                // JSONObject json = readJsonFromUrl("https://graph.facebook.com/19292868552");
-                // JSONObject json = readJsonFromUrl("https://cnn.com");
-                // System.out.println(reqString);
-                JSONArray json = readJsonFromUrl(reqString);
-                System.out.println("Finished getting json.");
-                if (json != null)
-                    System.out.println(json.toString());
-
-                if (changeListener != null)
-                    changeListener.netResult(0, json);
-
-                //System.out.println("Notify that JSON has come in");
-                // if (noteConnector != null)
-                //    noteConnector.ncnotify(0, "");
-
-            } catch (IOException e) {
-                System.out.println("IO exception");
-                //System.out.println(e);
-                if (changeListener != null)
-                    changeListener.netResult(1, null);
-            } catch (JSONException e) {
-                System.out.println("JSON Didn't work");
-                //System.out.println(e);
-                if (changeListener != null)
-                    changeListener.netResult(2, null);
-            }
-            return true;
-        }
-
-        private String readAll(Reader rd) throws IOException {
-            StringBuilder sb = new StringBuilder();
-            int cp;
-            while ((cp = rd.read()) != -1) {
-                sb.append((char) cp);
-            }
-            System.out.println("Read from the URL");
-            System.out.println(sb.toString());
-            System.out.println("Going to try to turn it into json");
-            return sb.toString();
-        }
-
-        public JSONArray readJsonFromUrl(String request) throws IOException, JSONException {
-            URL nurl = new URL(urlString);
-
-            HttpURLConnection connection = (HttpURLConnection) nurl.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            System.out.println("Network request to " + urlString + " with request " + reqString);
-            OutputStream urlout = connection.getOutputStream();
-            System.out.println("here");
-
-            //String s = "id=3452&second=fjfjfjfj";
-            urlout.write(request.getBytes());
-            urlout.close();
-            InputStream is = connection.getInputStream();
-
-            System.out.println("Waiting for network stream");
-            try {
-                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-                String jsonText = readAll(rd);
-                System.out.println("JSON is " + jsonText);
-
-                JSONArray jarray = new JSONArray(jsonText);
-
-
-                System.out.println("Got the object");
-                return jarray;
-            } finally {
-                is.close();
-                // System.out.println("Did not get the object.");
+            NetTask(String url, String request, NetResponse responseListener) {
+                urlString = url;
+                reqString = request;
+                changeListener = responseListener;
             }
 
+            @Override
+            protected Boolean doInBackground(Void... params) {
+                try {
+                    System.out.println("JSON Query: " + reqString);
+                    // JSONObject json = readJsonFromUrl("https://graph.facebook.com/19292868552");
+                    // JSONObject json = readJsonFromUrl("https://cnn.com");
+                    // System.out.println(reqString);
+                    JSONArray json = readJsonFromUrl(reqString);
+                    System.out.println("Finished getting json.");
+                    if (json != null)
+                        System.out.println(json.toString());
 
+                    if (changeListener != null)
+                        changeListener.netResult(0, json);
+
+                    //System.out.println("Notify that JSON has come in");
+                    // if (noteConnector != null)
+                    //    noteConnector.ncnotify(0, "");
+
+                } catch (IOException e) {
+                    System.out.println("IO exception");
+                    System.out.println(e);
+                    if (changeListener != null)
+                        changeListener.netResult(1, null);
+                } catch (JSONException e) {
+                    System.out.println("JSON Didn't work");
+                    //System.out.println(e);
+                    if (changeListener != null)
+                        changeListener.netResult(2, null);
+                }
+                return true;
+            }
+
+            private String readAll(Reader rd) throws IOException {
+                StringBuilder sb = new StringBuilder();
+                int cp;
+                while ((cp = rd.read()) != -1) {
+                    sb.append((char) cp);
+                }
+                System.out.println("Read from the URL");
+                System.out.println(sb.toString());
+                System.out.println("Going to try to turn it into json");
+                return sb.toString();
+            }
+
+            public JSONArray readJsonFromUrl(String request) throws IOException, JSONException {
+                URL nurl = new URL(urlString);
+
+                HttpURLConnection connection = (HttpURLConnection) nurl.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                System.out.println("Network request to " + urlString + " with request " + reqString);
+                OutputStream urlout = connection.getOutputStream();
+                System.out.println("here");
+
+
+
+
+                InputStream is = connection.getInputStream();
+
+                System.out.println("Waiting for network stream");
+                try {
+                    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                    String jsonText = readAll(rd);
+                    System.out.println("JSON is " + jsonText);
+
+                    JSONArray jarray = new JSONArray(jsonText);
+
+
+                    System.out.println("Got the object");
+                    return jarray;
+                } finally {
+                    is.close();
+                    // System.out.println("Did not get the object.");
+                }
+
+
+            }
         }
+
     }
-
